7a335814158f9a123eb303c40f0b77a320d2643a
jessebrennan <brennan@ucsc.edu>
2020-04-11 20:37:01 -0700

    Do local imports for smoke test

--- test/test_smoke.py
+++ test/test_smoke.py
@@ -1,22 +1,29 @@
+import logging
 import unittest
 
 import cutest
-# FIXME: import should not be global
-from test import sample
+
+logging.basicConfig(
+    format="%(asctime)s %(levelname)-7s %(threadName)s: %(message)s",
+    level=logging.INFO
+)
 
 
 class SmokeTest(unittest.TestCase):
 
     def test_sample_module(self):
+        from test import sample
         runner = cutest.SerialRunner()
         sample.cu.initialize()
         runner.run_model(sample.cu)
 
     def test_sample_suite(self):
+        from test import sample
         runner = cutest.SerialRunner()
         sample.my_suite.initialize()
         runner.run_suite(sample.my_suite)
 
     def test_sample_tests(self):
+        from test import sample
         runner = cutest.SerialRunner()
         runner.run_tests(sample.test_1.calls)
